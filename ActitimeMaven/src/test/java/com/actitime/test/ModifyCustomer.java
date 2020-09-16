ef5ae8eec30b438100cd4aded13bfb34e746a6ef
LENOVO <LENOVO@MAHABOOB>
2020-09-16 12:33:58 +0530

    Actitime Maven

---
+++ ActitimeMaven/src/test/java/com/actitime/test/ModifyCustomer.java
@@ -0,0 +1,13 @@
+package com.actitime.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ModifyCustomer {
+	@Test
+	public void testmodifycustomer()
+	{
+		Reporter.log("modify customer",true);
+	}
+
+}
